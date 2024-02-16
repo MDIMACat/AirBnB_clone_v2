e3dcd728a96d44d3fd2d6d42cf0d7f5af7633a9f
MDIMACat <romanmdima@gmail.com>
2024-02-16 08:03:31 +0200

    CHanged task 2 file format

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,20 +4,18 @@
 
 from flask import Flask
 from markupsafe import escape
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def greeting():
-    """Greeting method
-    """
+    """Greeting method"""
     return "Hello HBNB!"
 
 @app.route('/hbnb')
 def hbnb():
-    """HBNB METHOD
-    """
+    """HBNB METHOD"""
     return "HBNB"
 
 @app.route('/c/<text>')
@@ -29,4 +27,4 @@ def c_text(text):
     return f"C {escape(text)}!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port='5000')
