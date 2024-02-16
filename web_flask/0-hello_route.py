b996b548684e248b9e8042fad674c8145a90dadd
MDIMACat <romanmdima@gmail.com>
2024-02-16 07:57:55 +0200

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,15 +4,13 @@ a script that starts a Flask web application
 """
 
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def greeting():
-    """
-    Greeting method
-    """
+    """Greeting method"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
