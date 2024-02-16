113fa9195317df91187807ce549c084beb20633e
MDIMACat <romanmdima@gmail.com>
2024-02-16 08:00:56 +0200

    Changed file format

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,20 +3,19 @@
 """
 
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def greeting():
-    """Greeting method
-    """
+    """Greeting method"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """HBNB METHOD
-    """
+    """HBNB METHOD"""
     return "HBNB"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port='5000')
